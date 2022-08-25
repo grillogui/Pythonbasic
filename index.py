9c1ed397ef93da12a0ac18241ca4fbf4805eca30
grillogui <grilloalencar@gmail.com>
2022-08-25 08:28:57 -0300

    basic

---
+++ index.py
@@ -0,0 +1,34 @@
+print("olá mundo")
+
+ 
+nome = "guilherme"
+
+
+if nome == 'guilherme':
+    print("verdadeiro")
+else:
+    print("falso")
+
+
+nomes = ["gui","cabs","Lu"]
+print(nomes[0])
+
+for nome in nomes:
+    print(nome)
+
+
+i=0
+while i<10:
+    print(i)
+    i=i+1
+
+
+
+nominho = "guigo"
+
+if nominho == "joão":
+    print("falso")
+elif nominho == "gui":
+    print("falso")
+elif nominho == "guigo":
+    print("verdadeiro")
